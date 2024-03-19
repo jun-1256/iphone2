60a5bb3a99a4220926cd42f7cf681e4ba7ddeda6
jun <jun@localhost>
2024-03-19 20:45:22 +0900

    2024/03/19

---
+++ src/jp/co/diworks/action/NewFunction.java
@@ -0,0 +1,13 @@
+package jp.co.diworks.action;
+
+public interface NewFunction {
+	
+	public abstract void call();
+	
+	public abstract void mail();
+	
+	public abstract void phone();
+	
+	public abstract void internet();
+
+}
